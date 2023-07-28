43a0d6e8a93000f58e3d29a6e58fb79b9b7459f0
alimano <aliaizza@DESKTOP-UQHSB0V.localdomain>
2023-07-28 23:59:53 +0200

    game

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import './App.css';
 
 let paddlepos1:number;
 let paddlepos2:number = 0;
-let halfpaddle = 53;
+let halfpaddle = 3;
 let ballposy:number;
 let ballposx:number;
-// let Ballspeed:number = 8;
-// let leftscore:number = 0;
-// let rightscore:number = 0;
 
 const Score = ({ leftScore, rightScore }: { leftScore: number; rightScore: number }) => {
   const leftScoreStyle: React.CSSProperties = {
@@ -17,7 +14,7 @@ const Score = ({ leftScore, rightScore }: { leftScore: number; rightScore: numbe
     top: '0',
     textAlign: 'center',
     color: '#E15253',
-    fontSize: '30em',
+    fontSize: '30rem',
     paddingTop: '5%',
     fontFamily: 'Arial, sans-serif',
     zIndex: 1,
@@ -30,7 +27,7 @@ const Score = ({ leftScore, rightScore }: { leftScore: number; rightScore: numbe
     top: '0',
     textAlign: 'center',
     color: '#5699AF',
-    fontSize: '30em',
+    fontSize: '30rem',
     paddingTop: '5%',
     fontFamily: 'Arial, sans-serif',
     zIndex: 1,
@@ -50,17 +47,16 @@ const Score = ({ leftScore, rightScore }: { leftScore: number; rightScore: numbe
 };
 
 
-
 const Paddle = ({ color, pos }: { color: string; pos: string }) => {
   const paddleStyle: React.CSSProperties = {
-    width: '22px',
-    height: '106px',
+    width: '1.375rem', // 22px
+    height: '6.625rem', // 106px
     backgroundColor: color,
     position: 'relative',
     top: pos,
-    boxShadow: `0 0 20px ${color}`,
-    borderRadius: '20px',
-    marginInline: '20px',
+    boxShadow: `0 0 1.25rem ${color}`, // 20px
+    borderRadius: '1.25rem', // 20px
+    marginInline: '1.25rem', // 20px
     zIndex: 6,
   };
 
@@ -73,14 +69,14 @@ const Ball = ({color, setLeftScore, setRightScore, Ballspeed, setBallspeed, setG
   const animationFrameId = React.useRef<number>();
 
   const ballStyle: React.CSSProperties = {
-    width: '25px',
-    height: '25px',
+    width: '1.5625rem', // 25px
+    height: '1.5625rem', // 25px
     backgroundColor: color,
     borderRadius: '50%',
     position: 'relative',
-    left: `${ballPos.x}px`,
-    top: `${ballPos.y}px`,
-    boxShadow: '0 0 20px white'
+    left: `${ballPos.x}rem`, 
+    top: `${ballPos.y}rem`, 
+    boxShadow: '0 0 1.25rem white' // 20px
   };
 
   const ballSpeedRef = React.useRef(Ballspeed);
@@ -97,19 +93,19 @@ const Ball = ({color, setLeftScore, setRightScore, Ballspeed, setBallspeed, setG
         let newY = prevPos.y - (ballSpeedRef.current * Math.sin(moveAngle.current));
         ballposy = newY;
         ballposx = newX;
-        if (newX < -535 && (newY < paddlepos1 + halfpaddle && newY > paddlepos1 - halfpaddle) ){
-          newX = -535;
+        if (newX < -535 / 16 && (newY < paddlepos1 + halfpaddle && newY > paddlepos1 - halfpaddle) ){
+          newX = -535 / 16;
           moveAngle.current = Math.PI - moveAngle.current;
-          setBallspeed((prevspeed:number) => prevspeed + 0.5);
+          setBallspeed((prevspeed:number) => prevspeed + 0.3 / 16);
         }
-        if (newX > 540 && (newY < paddlepos2 + halfpaddle && newY > paddlepos2 - halfpaddle) ){
-          newX = 540;
+        if (newX > 540 / 16 && (newY < paddlepos2 + halfpaddle && newY > paddlepos2 - halfpaddle) ){
+          newX = 540 / 16;
           moveAngle.current = Math.PI - moveAngle.current;
-          setBallspeed((prevspeed:number) => prevspeed + 0.5);
+          setBallspeed((prevspeed:number) => prevspeed + 0.3 / 16);
         }
         // han fin katmarka koraaaaaaaaa
-        if ((newX < -575 || newX > 580)) {
-          if (newX < -575){
+        if ((newX < -575 / 16 || newX > 580 / 16)) {
+          if (newX < -575 / 16){
             setRightScore((prevScore: number) => {
               const newScore = prevScore + 1;
               if (newScore / 2 == 5) {
@@ -117,9 +113,9 @@ const Ball = ({color, setLeftScore, setRightScore, Ballspeed, setBallspeed, setG
               }
               return newScore;
             });
-            setBallspeed(() => 5);
+            setBallspeed(() => 5 / 16);
           }
-          else if (newX > 580){
+          else if (newX > 580 / 16){
             setLeftScore((prevScore: number) => {
               const newScore = prevScore + 1;
               if (newScore / 2 == 5) {
@@ -127,15 +123,16 @@ const Ball = ({color, setLeftScore, setRightScore, Ballspeed, setBallspeed, setG
               }
               return newScore;
             });
-            setBallspeed(() => 5);
+            setBallspeed(() => 5 / 16);
           }
           newX = 0;
           newY = 0;
         }
 
-        if (newY < -320 || newY > 325) {
-          newY = newY < -320 ? -320 : 325; 
-          moveAngle.current = -moveAngle.current; 
+        if (newY < -320 / 16 || newY > 325 / 16) {
+          newY = newY < -320 / 16 ? -320 / 16 : 325 / 16; 
+          moveAngle.current = -moveAngle.current;
+          setBallspeed((prevspeed:number) => prevspeed + 0.3 / 16);
         }
 
         return {x: newX, y: newY};
@@ -163,28 +160,19 @@ function App() {
   const movePaddle = React.useRef(0);
 
   const [secondPaddlePos, setSecondPaddlePos] = React.useState(0);
-  const moveBluePaddle = React.useRef(0);
 
   const [leftscore, setLeftScore] = React.useState(0);
   const [rightscore, setRightScore] = React.useState(0);
 
-  const [Ballspeed, setBallspeed] = React.useState(5);
+  const [Ballspeed, setBallspeed] = React.useState(5 / 16);
 
   const [gameOver, setGameOver] = React.useState(false);
 
-
-  const movecpu = () => {
-    if (secondPaddlePos < ballposy)
-      moveBluePaddle.current = 0.5;
-    else if (secondPaddlePos > ballposy)
-      moveBluePaddle.current = -0.5;
-  }
-
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'ArrowUp' || e.key === 'w') {
-        movePaddle.current = -2.5; // lfou9
+        movePaddle.current = -0.2;
     } else if (e.key === 'ArrowDown' || e.key === 's') {
-      movePaddle.current = 2.5; // lte7t
+      movePaddle.current = 0.2;
     }
   };
 
@@ -197,8 +185,8 @@ function App() {
       if (!gameOver) {
         setFirstPaddlePos((prev) => {
           const newPosition = prev + movePaddle.current;
-          const maxPos = 280; 
-          const minPos = -275; 
+          const maxPos = 17.5;  // 280px
+          const minPos = -17.1875;  // -275px
           
           paddlepos1 = Math.min(Math.max(newPosition, minPos), maxPos);
           return paddlepos1;
@@ -220,12 +208,12 @@ function App() {
       };
     }, [gameOver]);
 
-  React.useEffect(() => {
-    const updateSecondPaddlePosition = () => {
-      if (!gameOver) {
-        setSecondPaddlePos((prev) => {
-          let newPosition;
-          let cpuspeed:number = 6;
+    React.useEffect(() => {
+      const updateSecondPaddlePosition = () => {
+        if (!gameOver) {
+          setSecondPaddlePos(() => {
+            let newPosition;
+            let cpuspeed:number = 0.4375; // 7px
           newPosition = secondPaddlePos;
           if (ballposx > 0)
           if (secondPaddlePos < ballposy){
@@ -240,8 +228,8 @@ function App() {
             else
             newPosition = secondPaddlePos - cpuspeed;
           }
-          const maxPos = 280;
-          const minPos = -275;
+          const maxPos = 17.5;  // 280px
+          const minPos = -17.1875;  // -275px
           paddlepos2 = Math.min(Math.max(newPosition, minPos), maxPos);
           return paddlepos2;
           
@@ -255,9 +243,9 @@ function App() {
 
   return (
     <div className='table'>
-      <Paddle color="#E15253" pos={`${firstPaddlePos}px`} />
+      <Paddle color="#E15253" pos={`${firstPaddlePos}rem`} />
       <Ball color='white' setLeftScore={setLeftScore} setRightScore={setRightScore} Ballspeed={Ballspeed} setBallspeed={setBallspeed} setGameOver={setGameOver} gameOver={gameOver}/>
-      <Paddle color="#5699AF" pos={`${secondPaddlePos}px`} />
+      <Paddle color="#5699AF" pos={`${secondPaddlePos}rem`} />
       <Score leftScore={leftscore / 2} rightScore={rightscore / 2} />
       <div className="lineC">
         <div className="line"></div>
